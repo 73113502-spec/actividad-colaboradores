23aa65b2992f4f9f78ca76e98d8f2ac3e956dfe0
75535671-eng <75535671@continental.edu.pe>
2026-04-13 12:45:02 -0500

    Create primerPrograma.js

---
+++ primerPrograma.js
@@ -0,0 +1,12 @@
+let primerValor = 80;
+let segundoValor = 50;
+let resultado = 0;
+resultado = primerValor + segundoValor;
+if (resultado >= 100) {
+    console.log("Resultado de tres cifras");
+}
+else {
+    console.log("El resultado es menor a 100");
+}
+console.log(resultado);
+export {};
