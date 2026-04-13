d00cb591190154a40ae227dc2e4a285642ca9b17
75535671-eng <75535671@continental.edu.pe>
2026-04-13 12:45:03 -0500

    Create PrimerPrograma.ts

---
+++ PrimerPrograma.ts
@@ -0,0 +1,13 @@
+let primerValor: number = 80
+let segundoValor: number = 50
+let resultado: number = 0
+
+resultado = primerValor + segundoValor
+if (resultado >= 100) {
+    console.log("Resultado de tres cifras")
+}
+else {
+    console.log("El resultado es menor a 100")
+}
+console.log(resultado)
+export {}
