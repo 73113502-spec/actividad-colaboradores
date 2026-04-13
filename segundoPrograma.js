83c6c7e2fbb5c55fd587dec161fd336eca01cd00
75535671-eng <75535671@continental.edu.pe>
2026-04-13 12:45:07 -0500

    Create segundoPrograma.js

---
+++ segundoPrograma.js
@@ -0,0 +1,7 @@
+let usuarios = [];
+usuarios.push({ nombre: "Malu", edad: 22, contraseña: "2301" });
+usuarios.push({ nombre: "Maria", edad: 30, contraseña: "2301" });
+usuarios.push({ nombre: "Rosa", edad: 25, contraseña: "2301" });
+usuarios.push({ nombre: "Pepito", edad: 28, contraseña: "2301" });
+console.log(usuarios);
+export {};
